db5c2c770709fbe6d4f42d9d8492dc8d377f9e39
Zach Byers <byerszach@gmail.com>
2018-01-05 10:25:06 -0700

    output and sorting

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+ruby '2.3.1'
+
+require 'pry'
+require 'colorize'
---
+++ ascii_letters.rb
@@ -0,0 +1,74 @@
+require 'pry'
+require 'colorize'
+
+def ascii_hasher(input)
+  ascii_array = []
+
+  input.each { |char| ascii_array << { char => char.ord } }
+
+  ascii_array
+end
+
+def sort_menu
+  while true
+    puts "Sort? (y/n)".yellow
+
+    choice = gets.strip.downcase
+
+    case choice
+      when 'y'
+        return true
+      when 'n'
+        return false
+      else
+        puts "Invalid input".red
+    end
+  end
+end
+
+def sorter(arr)
+  while true
+    puts """
+    1. Ascending
+    2. Descending
+    """.yellow
+
+    input = gets.to_i
+
+    case input
+      when 1
+        return arr.sort_by { |hash| hash.values }
+      when 2
+        return arr.sort_by { |hash| hash.values }.reverse
+      else
+        puts "Invalid input.".red
+    end
+  end
+end
+
+while true
+  puts "Input some letters, separated by spaces.".yellow
+  input = gets.strip.split(' ')
+
+  output = ascii_hasher(input)
+
+  puts output
+
+  if sort_menu
+    puts sorter(output)
+  end
+
+  puts "Continue? (y/n)"
+
+  choice = gets.strip.downcase
+
+  case choice
+    when 'y'
+    when 'n'
+      puts "Buh-bye now".light_blue
+      exit 0
+    else
+      puts "Invalid input, so we're continuing anyway!".green
+  end
+
+end
